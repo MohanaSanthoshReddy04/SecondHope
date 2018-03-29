26ac8eb6629682ab0c83d88673e3c66aa3216e58
JankiRD <35822674+JankiRD@users.noreply.github.com>
2018-03-29 09:59:51 -0400

    Add files via upload

---
+++ Data/DashboardData.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Surrogacy.Helper;
+using Surrogacy.Models;
+
+namespace Surrogacy.Data
+{
+    public class DashboardData : BaseData
+    {
+        public DataSet ViewDashboardData(Dashboard dashboard)
+        {
+            DataSet dataSet = new DataSet();
+            try
+            {
+                string storedProcedure = "pSRGg_EmailInfo";
+                string parameterName = "@aXMLString";
+                string parameterValue = ObjectHelper.GetXMLFromObject(dashboard);
+                sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
+                sqlCommand.Parameters.AddWithValue(parameterName, parameterValue);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlConnection.Open();
+                sqlDataAdapter.SelectCommand = sqlCommand;
+                sqlDataAdapter.Fill(dataSet);
+                dataSet.Tables[0].TableName = "Dashboard";
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+            return dataSet;
+        }
+
+        public DataSet ViewDashboardComposeData(Dashboard dashboard)
+        {
+            DataSet dataSet = new DataSet();
+            try
+            {
+                string storedProcedure = "pSRGs_ComposeInfo";
+                string parameterName = "@aXMLString";
+                string parameterValue = ObjectHelper.GetXMLFromObject(dashboard);
+                sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
+                sqlCommand.Parameters.AddWithValue(parameterName, parameterValue);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlConnection.Open();
+                sqlDataAdapter.SelectCommand = sqlCommand;
+                sqlDataAdapter.Fill(dataSet);
+                dataSet.Tables[0].TableName = "Dashboard";
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+            return dataSet;
+        }
+    }
+}
---
+++ Data/SurrogateParentData.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Surrogacy.Helper;
+using Surrogacy.Models;
+
+namespace Surrogacy.Data
+{
+    public class SurrogateParentData : BaseData
+    {
+        public DataSet ViewSurrogateParentData(SurrogateParent surrogateparent)
+        {
+            DataSet dataSet = new DataSet();
+            try
+            {
+                string storedProcedure = "pSRGs_SurrogateParentInfo";
+                string parameterName = "@aXMLString";
+                string parameterValue = ObjectHelper.GetXMLFromObject(surrogateparent);
+                sqlCommand = new SqlCommand(storedProcedure, sqlConnection);
+                sqlCommand.Parameters.AddWithValue(parameterName, parameterValue);
+                sqlCommand.CommandType = CommandType.StoredProcedure;
+                sqlConnection.Open();
+                sqlDataAdapter.SelectCommand = sqlCommand;
+                sqlDataAdapter.Fill(dataSet);
+                dataSet.Tables[0].TableName = "SurrogateParent";
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            finally
+            {
+                sqlConnection.Close();
+            }
+
+            return dataSet;
+        }
+    }
+}
