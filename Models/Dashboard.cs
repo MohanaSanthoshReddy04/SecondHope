a007a88ab43d93507cd457fb453bdaf90969dcaa
JankiRD <35822674+JankiRD@users.noreply.github.com>
2018-03-29 09:36:01 -0400

    Add files via upload

---
+++ Models/Dashboard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Surrogacy.Entity;
+using Surrogacy.Util;
+
+namespace Surrogacy.Models
+{
+    public class Dashboard : BaseEntity
+    {
+        public string UserID { get; set; }
+        public int EmailBoxID { get; set; }
+        public string UserRoleDetail { get; set; }
+        public string FolderType { get; set; }
+        public string EmailFrom { get; set; }
+        public string EmailTo { get; set; }
+        public string EmailCC { get; set; }
+        public string EmailBCC { get; set; }
+        public string EmailSubject { get; set; }
+        public string EmailBody { get; set; }
+        public int IsRead { get; set; }
+        public string ReceivedTime { get; set; }
+        public int EmailFromID { get; set; }
+        public int EmailToID { get; set; }
+    }                                                                                                               
+}
---
+++ Models/SurrogateParent.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.VisualStudio.DebuggerVisualizers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Surrogacy.Entity;
+using Surrogacy.Util;
+
+namespace Surrogacy.Models
+{
+    public class SurrogateParent : BaseEntity
+    {
+        public string UserID { get; set; }
+        public int SurrogateID { get; set; }
+        public int ParentID { get; set; }
+        public string SurroagateFirstName { get; set; }
+        public string SurrogateLastName { get; set; }
+        public string ParentFirstName { get; set; }
+        public string ParentLastName { get; set; }
+        public string DoctorName { get; set; }
+        public string SurrogateEmailID { get; set; }
+        public string ParentEmailID { get; set; }
+        public string DoctorEmailID { get; set; }
+        public string SurrogateAge { set; get; }
+    }
+}
